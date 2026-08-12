51c44f9ce6c733d938f025ac663d7181dfcd4153
Dana Ibrayeva <ibraevadana2002@gmail.com>
2026-08-12 13:50:17 +0300

    fix: add headless mode flags for CI execution

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverFactory {
 
@@ -12,14 +13,28 @@ public class DriverFactory {
             browserName = "chrome";
         }
 
+        boolean isHeadless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+
         switch (browserName.toLowerCase()) {
             case "firefox":
-                return new FirefoxDriver();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                if (isHeadless) {
+                    firefoxOptions.addArguments("-headless");
+                }
+                return new FirefoxDriver(firefoxOptions);
+
             case "chrome":
             default:
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--incognito");
-                return new ChromeDriver(options);
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--incognito");
+
+                if (isHeadless) {
+                    chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--no-sandbox");
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+                    chromeOptions.addArguments("--window-size=1920,1080");
+                }
+                return new ChromeDriver(chromeOptions);
         }
     }
 }
